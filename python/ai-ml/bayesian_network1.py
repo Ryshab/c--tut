a21d77a8474f4c06574247fb82f701872a24934f
Rishabh <rishabhydv2405@gmail.com>
2024-03-07 21:40:03 +0530

    New commit

--- python/ai-ml/bayesian_network1.py
+++ python/ai-ml/bayesian_network1.py
@@ -1,37 +1,40 @@
-from pomegranate import *
-a = DiscreteDistribution({'1': 1./10, '0': 9./10})
-c = DiscreteDistribution({'1': 1./2, '0': 1./2})
+# from pomegranate import *
+import numpy as np
+from pomegranate.distributions import *
+from pomegranate.bayesian_network import BayesianNetwork
+a = Categorical({'1': 1./10, '0': 9./10})
+c = Categorical({'1': 1./2, '0': 1./2})
 
-b = ConditionalProbabilityTable(
+b = ConditionalCategorical(
 [[ '0', '0', 0.1 ],
 [ '0', '1', 0.9 ],
 [ '1', '0', 0.6 ],
 [ '1', '1', 0.4 ]],[a])
-d = ConditionalProbabilityTable(
+d = ConditionalCategorical(
 [[ '0', '0', 0.4 ],
 [ '0', '1', 0.6 ],
 [ '1', '0', 0.7 ],
 [ '1', '1', 0.3 ]],[c])
 
-e = ConditionalProbabilityTable(
+e = ConditionalCategorical(
 [[ '0', '0', 0.8 ],
 [ '0', '1', 0.2 ],
 [ '1', '0', 0.9 ],
 [ '1', '1', 0.1 ]],[b])
 
-h = ConditionalProbabilityTable(
+h = ConditionalCategorical(
 [[ '0', '0', 0.3 ],
 [ '0', '1', 0.7 ],
 [ '1', '0', 0.5 ],
 [ '1', '1', 0.5 ]],[e])
 
-i = ConditionalProbabilityTable(
+i = ConditionalCategorical(
 [[ '0', '0', 0.3 ],
 [ '0', '1', 0.7 ],
 [ '1', '0', 0.2 ],
 [ '1', '1', 0.8 ]],[e])
 
-f = ConditionalProbabilityTable(
+f = ConditionalCategorical(
 [[ '0', '0', '1', 0.0 ],
 [ '0', '1', '1', 0.2 ],
 [ '1', '0', '1', 0.4 ],
@@ -41,7 +44,7 @@ f = ConditionalProbabilityTable(
 [ '1', '0', '0', 0.6 ],
 [ '1', '1', '0', 0.3 ]],[b,d])
 
-g = ConditionalProbabilityTable(
+g = ConditionalCategorical(
 [[ '0', '0', 0.9 ],
 [ '0', '1', 0.1 ],
 [ '1', '0', 0.1 ],
---
+++ python/tut2.py
@@ -0,0 +1,4 @@
+a=int(input("Enter the no.:"))
+for i in range(1,11):
+    # print(f"(int{a}) X {i} = {int(a)*i}")
+    print(f"{a} X {i} = {a*i}")
