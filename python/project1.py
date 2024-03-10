d6c61f64cbec654a1156dad431a9ca6d5deabeed
Rishabh <rishabhydv2405@gmail.com>
2024-03-10 22:10:40 +0530

    Same commit

--- python/project1.py
+++ python/project1.py
@@ -7,18 +7,17 @@ class Library:
         book_details=[name,author,year, price,serialnum]
         Library.books.append(book_details)
 
-    def rembook(s):
-        for i in range (num) :
-            if not books:
+    def rembook(self,s):
+        for i in range (len(Library.books)) :
+            if not Library.books:
                 print("Sorry! No books available")
-            if books[i][4]==s:
-                del books[i]
-                print(f"Book with serial number {s} was:\n{name}\nwritten by {author}\nreleased in {year}\nPrice {price} ")
+            if Library.books[i][4]==s:
+                del Library.books[i]
+                # print(f"Book with serial number {s} was:\n{name}\nwritten by {author}\nreleased in {year}\nPrice {price} ")
                 print("This book has been removed successfully")
                 break
             else :
                 continue
-
         print("Book not found")
         
     @staticmethod
@@ -38,5 +37,7 @@ a.addbook("Crime and punishment","Fyodor Dostoevsky",1756,25,110001)
 b.addbook("Atomic Habits","koi toh h",2014,14,110002)
 c.addbook("Meditation","Marcus Aurelius",12000,5,110003)
 a.display()
-print(Library.num)
-# print(Library.books)
+b=Library()
+b.rembook(110002)
+a.display()
+print(a.__dict__)
